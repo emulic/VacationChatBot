927835504bed9697cb9e1d51788882e13fe25d4c
Damir Dobric <ddobric@daenet.com>
2024-10-08 00:09:42 +0200

    rename

--- src/Daenet.LLMPlugin.TestConsole/TestConsole.cs
+++ src/Daenet.LLMPlugin.TestConsole/TestConsole.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Daenet.EmbeddingSearchApi.Services;
 using Daenet.LLMPlugin.Common;
 
 namespace Daenet.LLMPlugin.TestConsole
